6ef29f27cc06de2747926b635db8aad6bd006e96
unknown <desiredshellstate@gmail.com>
2022-12-15 20:47:20 +0100

    added success page on form submit

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -4,8 +4,12 @@ import React from 'react'
 function Success() {
 
   return (
-    <div>Succes
-      <p></p>
+    <div className="card w-96   image-full mx-auto">
+      <figure><img src="https://placeimg.com/400/225/arch" alt="success" /></figure>
+      <div className="card-body justify-self-center">
+        <h2 className="card-title text-3xl place-content-center">Success</h2>
+        <p>I will get back to you shortly</p>
+      </div>
     </div>
   )
 }
