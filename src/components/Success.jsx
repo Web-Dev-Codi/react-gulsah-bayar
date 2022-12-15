147e0231ede8eed7ef65c6f23400883bdaa8d367
unknown <desiredshellstate@gmail.com>
2022-12-15 20:57:59 +0100

    added success image

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import berlin3 from './assets/berlin3.jpg'
 
 
 function Success() {
 
   return (
-    <div className="card w-96   image-full mx-auto">
-      <figure><img src="https://placeimg.com/400/225/arch" alt="success" /></figure>
+    <div className="card w-96 border  mx-auto">
+      <figure><img src={berlin3} alt="success" /></figure>
       <div className="card-body justify-self-center">
         <h2 className="card-title text-3xl place-content-center">Success</h2>
-        <p>I will get back to you shortly</p>
+        <p className='text-center'>I will get back to you shortly</p>
       </div>
     </div>
   )
